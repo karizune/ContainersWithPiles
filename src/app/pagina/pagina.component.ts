8e07293c30f23557eed82a90cfcf2d7c4acf306c
Luan Chrystian Galan <luanchrystian2@gmail.com>
2020-10-01 19:46:17 -0300

    resolvendo bugs

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -7,33 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaComponent implements OnInit {
 
-  local1 = [];
-  local2 = [];
-  local3 = [];
-  local4 = [];
-
+  local1:Array<any> = [];
+  local2:Array<any> = [];
+  local3:Array<any> = [];
+  local4:Array<any> = [];
+  local0:Array<any> = [];
+  posicao:number;
   constructor(
-  ) { }
+    
+  ) { 
+    this.local0.push(this.local1, this.local2, this.local3, this.local4);
+  }
 
   ngOnInit(): void {
   }
 
   adicionaContainer(container:any){
-    let erro = 0;
-    if(this.local1.length < 3){
+    let erro:number = 0;
+    if((this.local1.length < 3) && (this.local1[this.local1.length -1] != container)){
       this.local1.push(container);
+      return 0;
     }
-    else if(this.local2.length < 3){
+
+    if(this.local2.length < 3 && (this.local2[this.local2.length -1] != container)){
       this.local2.push(container);
+      return 0;
     }
-    else if(this.local3.length < 3){
+
+    if(this.local3.length < 3 && (this.local3[this.local3.length -1] != container)){
       this.local3.push(container);
+      return 0;
     }
-    else if(this.local4.length < 3){
+
+    if(this.local4.length < 3 && (this.local4[this.local4.length -1] != container)){
       this.local4.push(container);
-    }
-    else{
-      window.alert("Impossível Empilhar!");
+      return 0;
     }
   }
 
